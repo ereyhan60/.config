8b2c754448f1658b344042197cfac559b5074c3d
ereyhan60 <ereyhan60@gmail.com>
2023-07-31 00:57:44 +0300

    Added: grep and ip commands --color=auto

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,11 @@ if status is-interactive
     alias cat='bat -pp'
     alias dbkali='distrobox-enter --root kali'
     alias ex='exit'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+    alias diff='diff --color=auto'
+    alias ip='ip --color=auto'
 end
 set -g fish_prompt_pwd_dir_length 0
 set TERM xterm-256color
